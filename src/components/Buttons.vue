b221e888518e93a1fe9113ff064154dc493316a1
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-01-31 00:35:09 +0400

    custom button for the project

---
+++ src/components/Buttons.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps({
+    bgColor:{
+        type:String,
+        default:null
+    },
+    title:{
+        type:String,
+        default:"Donate",
+    },
+    color:{
+        type:String,
+        default:'white'
+    }
+})
+
+</script>
+
+<template>
+    <button  :style="{backgroundColor:bgColor, color:color}" class="btn">{{ title }}</button>
+</template>
+
+<style scoped lang='scss'>
+@import '../assets/vars.scss';
+.btn{
+    padding: 12px 32px;
+    background-color: $black;
+    border-radius: 4px;
+}
+</style>

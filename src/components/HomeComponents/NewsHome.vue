a5198f1f362695f0fbfa82f327af55d66f52298f
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-02-12 01:13:14 +0400

    parent component for news cards

---
+++ src/components/HomeComponents/NewsHome.vue
@@ -0,0 +1,61 @@
+<script setup>
+import {ref} from 'vue'
+import NewsCard from '@/components/NewsCard.vue';
+
+const cards = [
+    {
+        images:'src/assets/images/newsCard1.png',
+        title:'Don’t destroy greenery and don’t spoil scenery',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim'
+    },
+    {
+        images:'src/assets/images/newsCard2.png',
+        title:' Is climate change happening faster than expected?',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim'
+    },
+    {
+        images:'src/assets/images/newsCard3.png',
+        title:"Top 10 facts about wind farms you didn't know",
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim'
+    },
+    {
+        images:'src/assets/images/newsCard4.png',
+        title:'Our goal is to make water available for everyone',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim'
+    },
+]
+
+</script>
+
+<template>
+    <section class="news">
+        <div class="container">
+            <h2 class="news__title">
+                Read Our News
+            </h2>
+            <div class="news-cards">
+                <NewsCard 
+                v-for="(card, index) in cards" :key="index"
+                :images="card.images"
+                :title="card.title"
+                :text="card.text"
+                >
+            </NewsCard>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang='scss'>
+@import '@/assets/vars.scss';
+.news{
+    &__title{
+        font-size: $heading2;
+    }
+    &-cards{
+        display: flex;
+        gap: 24px;
+        margin-top: 48px;
+    }
+}
+</style>

c52e6f0e6aec363c4447d94daf9324aad1a6cba4
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-02-17 21:27:13 +0400

    the event card component and its parent element

---
+++ src/components/Events.vue
@@ -0,0 +1,49 @@
+<script setup>
+import EventsCard from './EventsCard.vue';
+
+const cards = [
+    {
+        title:'Say no to plastic usage and save the planet',
+        month:'SEP',
+        monthDate:23,
+    },
+    {
+        title:'Weekly cleaning program',
+        month:'SEP',
+        monthDate:25,
+    }
+]
+
+
+</script>
+
+<template>
+    <section class="events">
+        <div class="container">
+            <h2 class="events__title">Our Events</h2>
+            <div class="events-cards">
+                <EventsCard v-for="({title,month,monthDate} = cards, index) in cards" :key="index"
+                :title="title"
+                :month="month"
+                :date="monthDate"
+                >
+            </EventsCard>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang='scss'>
+.events{
+    &-cards{
+        display: flex;
+        margin-bottom: 136px;
+        margin-top: 48px;
+        justify-content: space-between;
+    }
+    &__title{
+        font-size: 40px;
+    }
+}
+
+</style>
---
+++ src/components/EventsCard.vue
@@ -0,0 +1,75 @@
+<script setup>
+const props = defineProps({
+    date:{
+        type:Number,
+        default:'0',
+        required:true
+    },
+    month:{
+        type:String,
+        default:'Sep',
+        required:true
+    },
+    title:{
+        type:String,
+        default:'Say no to plastic usage and save the planet',
+        required:true
+    },
+    
+})
+
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-date">
+            <h2 class="card-date__num">{{ props.date }}</h2>
+            <p class="card-date__month">{{ props.month }}</p>
+        </div>
+        <div class="card-content">
+            <p class="card-content__paragraph">Next Events</p>
+            <h3 class="card-content__title">{{ props.title }}</h3>
+        </div>
+        <button class="card__btn"></button>
+    </div>
+</template>
+
+<style scoped lang='scss'>
+@import '@/assets/vars.scss';
+
+.card{
+    display: flex;
+    justify-content: space-between;
+    background-color: $secondary-green;
+    align-items: start;
+    padding: 40px;
+    gap: 24px;
+    border-radius: 8px;
+    &-date{
+        margin-top: -10px;
+            &__num{
+                font-size: $heading2;
+            }
+        }
+    &-content{
+        width:381px;
+        &__paragraph{
+            @include text-line(right,50%)
+        }
+        &__title{
+            font-size: $heading3;
+            line-height: 42px;
+            margin-top: 8px;
+        } 
+    }
+    &__btn{
+        background:center no-repeat url(src/assets/images/Arrow.svg) $white;
+        width: 56px;
+        height: 56px;
+        border-radius: 50%;
+        margin: auto;
+    }
+}
+
+</style>

03d64ac23e31a0f44f9d94d116387308fecaf28d
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-02-17 21:30:40 +0400

    connecting components to a page

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -4,8 +4,11 @@ import Banner from '@/components/HomeComponents/Banner.vue';
 import AboutHome from '@/components/HomeComponents/AboutHome.vue';
 import Deeds from '@/components/HomeComponents/Deeds.vue';
 import ProjectsHome from '@/components/HomeComponents/ProjectsHome.vue';
-import Donation from '@/components/HomeComponents/Donation.vue';
+import Diagram from '@/components/HomeComponents/Diagram.vue';
 import NewsHome from '@/components/HomeComponents/NewsHome.vue';
+import Events from '@/components/Events.vue';
+import Footer from '@/components/Footer.vue';
+import Donate from '@/components/Donate.vue';
 </script>
 
 <template>
@@ -14,8 +17,11 @@ import NewsHome from '@/components/HomeComponents/NewsHome.vue';
     <AboutHome></AboutHome>
     <Deeds></Deeds>
     <ProjectsHome></ProjectsHome>
-    <Donation></Donation>
+    <Diagram></Diagram>
+    <Donate></Donate>
     <NewsHome></NewsHome>
+    <Events></Events>
+    <Footer></Footer>
 </template>
 
 <style scoped lang='scss'>

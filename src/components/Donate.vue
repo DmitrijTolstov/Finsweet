f9f25302b623d206d391f7965d9712b23f2ed48e
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-02-17 21:26:35 +0400

    the component of donations

---
+++ src/components/Donate.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Buttons from './Buttons.vue';
+</script>
+
+<template>
+    <div class="container">
+        <section class="donate">
+            <div class="donate-content">
+                <h2 class="donate-content__title">
+                    You can contribute to make the environment greener!
+                </h2>
+                <div class="donate-content_buttons">
+                    <Buttons :bgColor="'#70C174'" :title="'Join as a volunteer'"></Buttons>
+                    <Buttons :bgColor="'white'" :color="'#1D2130'"></Buttons>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style scoped lang='scss'>
+@import '@/assets/vars.scss';
+.donate{
+    background: center no-repeat url(src/assets/images/background-donate.png);
+    padding: 93px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 12px;
+    margin-bottom: 96px;
+    &-content{
+        text-align: center;
+        &__title{
+            font-size: $heading2;
+            color: $white;
+            max-width: 631px;
+        }
+        &_buttons{
+            margin-top: 32px;
+            display: inline-flex;
+            gap: 32px;
+        }
+    }
+}
+</style>

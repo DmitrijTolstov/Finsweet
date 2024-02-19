e1251206c05f14fe9583a2c0927393273de6b71d
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-02-20 00:42:10 +0400

    the about us page

---
+++ src/layouts/About.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Footer from '@/components/Footer.vue';
+import Header from '@/components/Header.vue';
+import AboutMainPage from '@/components/aboutComponents/AboutMainPage.vue';
+</script>
+
+<template>
+    <Header></Header>
+    <AboutMainPage></AboutMainPage>
+    <Footer></Footer>
+
+</template>
+
+<style scoped lang='scss'>
+</style>

44a0733f35d04267c39adbe82fe13f6093ed557e
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-03-06 22:09:14 +0400

    a page with cases for the environment

---
+++ src/components/WhatWeDo/WhatWeDoMainPage.vue
@@ -0,0 +1,53 @@
+<script setup>
+</script>
+
+<template>
+    <section class="WhatWeDo">
+        <div class="container">
+            <div class="WhatWeDo-container">
+                <div class="WhatWeDo-content">
+                    <p class="WhatWeDo-content__heading">
+                        What We Do 
+                    </p>
+                        <h2 class="WhatWeDo-content__title">
+                            We are working <hr> worldwide
+                        </h2>
+                        <p class="WhatWeDo-content__text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+                        </p>
+                </div>
+                <div class="whTWeDo_image">
+                    <img src="@/assets/images/WhatWeDo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang='scss'>
+@import '@/assets/vars.scss';
+
+.WhatWeDo{
+    margin-bottom: 96px;
+    &-container{
+        display:flex;
+        justify-content: space-between;
+        gap: 110px;
+    }
+    &-content{
+        &__heading{
+            @include text-line();
+            margin-bottom: 32px;
+        }
+        &__title{
+            font-size: $heading2;
+            margin-bottom: 34px;
+            font-weight: 700;
+        }
+        &__text{
+            opacity: .4;
+        }
+    }
+}
+
+</style>
---
+++ src/layouts/WhatWeDo.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Header from '@/components/Header.vue';
+import Environment from '@/components/WhatWeDo/Environment.vue';
+import Projects from '@/components/WhatWeDo/Projects.vue';
+import Donate from '@/components/Donate.vue';
+import News from '@/components/News.vue';
+import Events from '@/components/Events.vue';
+import Footer from '@/components/Footer.vue';
+import WhatWeDoMainPage from '@/components/WhatWeDo/WhatWeDoMainPage.vue';
+
+
+</script>
+
+<template>
+        <Header></Header>
+        <WhatWeDoMainPage></WhatWeDoMainPage>
+        <Environment></Environment>
+        <Projects></Projects>
+        <Donate></Donate>
+        <News></News>
+        <Events></Events>
+        <Footer></Footer>
+</template>
+
+<style scoped lang='scss'>
+</style>

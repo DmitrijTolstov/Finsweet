86d2c776d7ecd6e3c5468fc520599fecd0a29fd3
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-03-19 08:59:51 +0400

    the news page

---
+++ src/components/MediaComponents/MediaNews.vue
@@ -0,0 +1,20 @@
+<script setup>
+import News from '@/components/News.vue';
+</script>
+
+<template>
+    <div class="news">
+        <News 
+                :title="'Read our Recent News'"
+                :text="'Edit this text to make it your own. To edit, simply click directly on the text to start We are served'"
+                :textShow="true"
+                >
+        </News>
+</div>
+</template>
+
+<style scoped lang='scss'>
+.news{
+    text-align:center;
+}
+</style>
---
+++ src/layouts/Media.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Events from '@/components/Events.vue';
+import Footer from '@/components/Footer.vue';
+import MainPage from '@/components/MediaComponents/MainPage.vue';
+import MediaNews from '@/components/MediaComponents/MediaNews.vue';
+
+
+</script>
+
+<template>
+        <MainPage></MainPage>
+        <MediaNews></MediaNews>
+        <Events></Events>
+        <Footer></Footer>
+</template>
+
+<style scoped lang='scss'>
+</style>

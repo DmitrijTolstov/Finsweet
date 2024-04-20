a7b27dd9a9b9bf0a428c485d42cdfec560c685aa
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-04-21 02:18:45 +0400

    adding a map and footer to the contact page

--- src/layouts/Contact.vue
+++ src/layouts/Contact.vue
@@ -1,13 +1,16 @@
 <script setup>
 import Footer from '@/components/Footer.vue';
 import MainPage from '@/components/ContactComponents/MainPage.vue';
-
+import Form from '@/components/Form.vue';
+import Maps from '@/components/ContactComponents/MapsContacts.vue'
 
 
 </script>
 
 <template>
     <MainPage></MainPage>
+    <Form></Form>
+    <Maps></Maps>
     <Footer></Footer>
 
 </template>

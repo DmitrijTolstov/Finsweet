d01407a30849a92e2ad2ede40f3c8034cca4501f
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-04-21 02:18:15 +0400

    the donation page

---
+++ src/layouts/Donate.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MainPage from '@/components/DonateComponents/MainPage.vue';
+import ContentDonate from '@/components/DonateComponents/ContentDonate.vue';
+import Footer from '@/components/Footer.vue';
+import Donate from '@/components/Donate.vue';
+
+
+</script>
+
+<template>
+    <MainPage></MainPage>
+    <ContentDonate></ContentDonate>
+    <Donate></Donate>
+    <Footer></Footer>
+</template>
+
+<style scoped lang='scss'>
+</style>

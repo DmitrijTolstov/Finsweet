09375e41dc3950290028504d39fc989a94598a39
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-04-21 02:21:08 +0400

    component for the project page

---
+++ src/components/ProjectsComponents/OurBusiness.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+const business = [
+    {
+        image:'src/assets/images/projects-icons/image1.png',
+        title:'200+ Plantation in 1 week',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+    },
+    {
+        image:'src/assets/images/projects-icons/image2.png',
+        title:'230 People Have donated',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+    },
+    {
+        image:'src/assets/images/projects-icons/image3.png',
+        title:'30+ people joined ',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+    },
+    {
+        image:'src/assets/images/projects-icons/image4.png',
+        title:'$324 Raised for this initiate',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.',
+    },
+]
+
+
+</script>
+
+<template>
+            <div class="ourBusiness-container">
+                <div class="ourBusiness-content" v-for="({title,text,image}, index) in business" :key="index">
+                    <div class="ourBusiness-content__header">
+                        <img width="28px" height="28px" :src="image" alt="">
+                        <h4 class="ourBusiness-content__title">{{ title }}</h4>
+                    </div>
+                    <p class="ourBusiness-content__text">{{ text }}</p>
+                </div>
+            </div>
+</template>
+
+<style scoped lang='scss'>
+
+.ourBusiness{
+    &-container{
+        margin: 0 auto;
+        max-width: 70%;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 48px;
+        justify-content: space-evenly;
+    }
+
+    &-content{
+        max-width:450px;
+        &__header{
+            display: flex;
+            margin-left: -54px;
+            gap: 26px;
+        }
+        &__title{
+            font-size:24px;
+            font-weight:700;
+            margin-bottom:8px;
+        }
+    }
+}
+</style>

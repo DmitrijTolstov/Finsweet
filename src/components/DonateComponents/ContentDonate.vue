b4a37c9d4d95f44810e1e3497c27631da0b07cda
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-04-21 02:19:21 +0400

    components of the donation page

---
+++ src/components/DonateComponents/ContentDonate.vue
@@ -0,0 +1,64 @@
+<script setup>
+import Accordion from '@/components/Accordion.vue';
+</script>
+
+<template>
+    <section class="content">
+        <div class="container">
+            <main class="content mb-192">
+                <div class="content-main">
+                    <h2 class="content-main__title">
+                        How you can contribute to protect Earth
+                    </h2>
+                    <p class="content-main__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+                    </p>
+                </div>
+                    <Accordion></Accordion>
+            </main>
+            <article class="content">
+                <h2 class="content__title">
+                    How we use your donation
+                </h2>
+                <p class="content__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.  Nunc ut sem vitae risus tristique posuere.
+                </p>
+                <p class="content__text">
+                    Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet.
+                </p>
+            </article>
+        </div>
+    </section>
+</template>
+
+<style scoped lang='scss'>
+
+@import '@/assets/vars.scss';
+
+.content{
+    display: flex;
+    margin-bottom: 96px;
+    gap: 96px;
+    &-main{
+        display: flex;
+        flex-direction: column;
+        max-width: 592px;
+        &__title{
+            font-size: $heading2;
+            font-weight: 700;
+            margin-bottom: 24px;
+        }
+    }
+    &__title{
+        font-size: $heading2;
+        font-weight: 700;
+    }
+    &__text{
+        width: 50%;
+    }
+}
+
+.mb-192{
+    margin-bottom: 192px;
+}
+</style>
---
+++ src/components/DonateComponents/MainPage.vue
@@ -0,0 +1,76 @@
+<script setup>
+import Buttons from '../Buttons.vue';
+import Header from '../Header.vue';
+
+
+</script>
+
+<template>
+    <section class="donate">
+        <Header></Header>
+        <div class="container">
+            <div class="donate-container">
+                <div class="donate-content">
+                        <p class="donate-content__heading">
+                            donate us
+                        </p>
+                        <div class="pl-96">
+                            <h2 class="donate-content__title">
+                                Making a donation for Nature.
+                            </h2>
+                            <p class="donate-content__text">
+                                When you donate, you’re supporting effective solutions to big environmental challenges—an investment in the future of our planet.
+                            </p>
+                            <Buttons :bg-color="'#70C174'" :title="'Donate now'"></Buttons>
+                        </div>
+                </div>
+                <article class="donate-image">
+                    <img src="@/assets\images\donate\Image.png" alt="men">
+                </article>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang='scss'>
+
+@import '@/assets/vars.scss';
+
+.donate{
+    background-color:$off-white;
+    padding-bottom: 96px;
+    margin-bottom: 96px;
+    &-container{
+        display:flex;
+        justify-content: space-between;
+        gap: 76px;
+    }
+    &-content{
+        &__heading{
+            @include text-line();
+            margin-bottom: 32px;
+        }
+        &__title{
+            font-size: $heading2;
+            margin-bottom: 34px;
+            font-weight: 700;
+        }
+        &__text{
+            opacity: .4;
+            margin-bottom: 32px;
+            max-width: 517px;
+        }
+    }
+    &-container{
+        display:flex
+    }
+    &-blogs_links{
+        margin: 32px 0 96px;
+        display:flex;
+        gap: 20px;
+    }
+}
+.pl-96{
+    padding-left: 96px
+}
+</style>
